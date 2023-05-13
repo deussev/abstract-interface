25b61060f8b32a9dae04d136960074e24582789b
unknown <rositeniel@gmail.com>
2023-05-13 14:10:29 +0800

    first commit

--- class.php
+++ class.php
@@ -1,6 +1,27 @@
 <?php
 
-abstract class model
+abstract class Database
 {
-    
+    public $conn;
+    public $servername = "localhost";
+    public $username = "root";
+    public $gitpassword = "";
+    public $dbName = "niel";
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->servername, $this->username, $this->password);
+    }
+    abstract public function dbName(): string;
 }
+
+class User extends Database
+{
+    public function dbName(): string 
+    {
+        $dbName = "CREATE DATABASE IF NOT EXIST $this->dbName";
+        return $this->conn->query($dbName);
+    }
+}
+
+$new = new User();
+$new->dbName();
